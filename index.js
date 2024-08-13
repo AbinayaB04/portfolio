6c40a92c7161010735424400dcec34bdf021a4ac
AbinayaB04 <128470293+AbinayaB04@users.noreply.github.com>
2024-08-13 23:23:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let type =new Typed("#changing-text",{
-    strings : ['Front end Developer','Cyber Security Enthusiasist'],
+    strings : ['Abinaya B','Front end Developer','Cyber Security Analyst'],
     typeSpeed:50,
     backSpeed:50,
     loop:true,
